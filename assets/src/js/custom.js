0f243e7e42ca8fc227d2ea9ea96443e17483720a
Sanyok <sanyokdb@gmail.com>
2020-12-14 18:16:41 +0500

    update

--- assets/src/js/custom.js
+++ assets/src/js/custom.js
@@ -21,9 +21,9 @@ $(function () {
     slidesToShow: 3,
     appendArrows:$(".post-arrows"),
     prevArrow:
-      '<button class="post-arrows__prev"><svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M136.97 380.485l7.071-7.07c4.686-4.686 4.686-12.284 0-16.971L60.113 273H436c6.627 0 12-5.373 12-12v-10c0-6.627-5.373-12-12-12H60.113l83.928-83.444c4.686-4.686 4.686-12.284 0-16.971l-7.071-7.07c-4.686-4.686-12.284-4.686-16.97 0l-116.485 116c-4.686 4.686-4.686 12.284 0 16.971l116.485 116c4.686 4.686 12.284 4.686 16.97-.001z" class=""></path></svg></button>',
+      '<button class="post-arrows__prev"><i class="icon-long-arrow-left"></i></button>',
     nextArrow:
-      '<button class="post-arrows__next"><svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M311.03 131.515l-7.071 7.07c-4.686 4.686-4.686 12.284 0 16.971L387.887 239H12c-6.627 0-12 5.373-12 12v10c0 6.627 5.373 12 12 12h375.887l-83.928 83.444c-4.686 4.686-4.686 12.284 0 16.971l7.071 7.07c4.686 4.686 12.284 4.686 16.97 0l116.485-116c4.686-4.686 4.686-12.284 0-16.971L328 131.515c-4.686-4.687-12.284-4.687-16.97 0z" class=""></path></svg></button>',
+      '<button class="post-arrows__next"><i class="icon-long-arrow-right"></i></button>',
     responsive: [
       {
         breakpoint: 991,
@@ -33,6 +33,24 @@ $(function () {
       },
       {
         breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
+  });
+
+  $(".reviews-slider").slick({
+    infinite: true,
+    slidesToShow: 2,
+    appendArrows:$(".reviews-arrows"),
+    prevArrow:
+      '<button class="reviews-arrows__prev"><i class="icon-long-arrow-left"></i></button>',
+    nextArrow:
+      '<button class="reviews-arrows__next"><i class="icon-long-arrow-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 767.98,
         settings: {
           slidesToShow: 1,
         }
